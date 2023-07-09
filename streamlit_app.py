e6cfc250701e79a9f33241ffc3e873ca64e03729
PongphonTan <pongphon.tan@gmail.com>
2023-07-10 00:34:15 +0700

    Fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,10 +54,11 @@ except URLError as e:
 # streamlit.text(my_data_row)
 
 streamlit.header('The fruit load list contains:')
+
 def get_fruit_load_list() -> pd.DataFrame:
     with my_cnx.cursor() as my_cur:
         my_cur.execute('select * from pc_rivery_db.public.fruit_load_list')
-        return my_cur.fetch_all()
+        return my_cur.fetchall()
 
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
