ccb94109832ad02aab750aa2a500acd2e71425d3
PongphonTan <pongphon.tan@gmail.com>
2023-07-10 00:11:14 +0700

    Can You Add A Second Text Entry Box?

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,11 +38,15 @@ streamlit.text('Hello from Snowflake:')
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text(my_data_row)
+# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+# my_data_row = my_cur.fetchone()
+# streamlit.text(my_data_row)
 
 streamlit.header('The fruit load list contains:')
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.dataframe(my_data_rows)
+
+fruit_choice = streamlit.text_input('What fruit would you like to add?')
+add_my_fruit = fruit_choice
+streamlit.write('Thanks for adding ', add_my_fruit)
